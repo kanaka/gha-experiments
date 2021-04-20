7e014a9ca9a5698f90ede25713e1606a1c672601
Joel Martin <github@martintribe.org>
2021-04-20 17:17:58 -0500

    Fix impls keys.

--- get-matrix.py
+++ get-matrix.py
@@ -4,5 +4,5 @@ import json, yaml
 
 impls = yaml.safe_load(open("impls.yml"))
 
-print("::set-output name=linux::{\"IMPL\":%s}" % json.dumps(impls[linux]))
-print("::set-output name=osx::{\"IMPL\":%s}" % json.dumps(impls[osx]))
+print("::set-output name=linux::{\"IMPL\":%s}" % json.dumps(impls['linux']))
+print("::set-output name=osx::{\"IMPL\":%s}" % json.dumps(impls['osx']))
